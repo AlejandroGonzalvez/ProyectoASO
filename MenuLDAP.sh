6274f2e4399237772cf4a5024e218e277ed3148b
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-10-28 19:12:32 +0100

    Update MenuLDAP.sh

--- MenuLDAP.sh
+++ MenuLDAP.sh
@@ -1,3 +1,4 @@
 #¡/bin/bash
-
-
+echo "Pulse 1 si quiere crear una nueva entrada en el directorio LDAP"
+echo "Pulse 2 si quiere modificar una entrada en el directorio LDAP"
+echo "Pulse 3 si quiere eliminar una entrada en el directorio LDAP"
