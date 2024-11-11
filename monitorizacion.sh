2bdda0600105c84830c82563eeab345a04496d69
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-11 19:41:29 +0100

    Create monitorizacion.sh

---
+++ monitorizacion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Identificar los cinco procesos que mas memoria concumen
+ps aux --sort=-%mem | head -n 6 | tail -n 5
+#Identificar los cinco procesos que mas CPU concumen
+ps aux --sort=-%cpu | head -n 6 | tail -n 5
