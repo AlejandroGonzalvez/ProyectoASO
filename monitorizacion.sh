7e2adeccd3c16bbcf718114cddcd0893f1d503b8
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-16 20:06:28 +0100

    Update monitorizacion.sh

--- monitorizacion.sh
+++ monitorizacion.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-#Identificar los cinco procesos que mas memoria consumen
-ps aux --sort=-%mem | head -n 6 | tail -n 5
-#Identificar los cinco procesos que mas CPU concumen
-ps aux --sort=-%cpu | head -n 6 | tail -n 5
+#Revisar los procesos que mas memoria consumen
+	#Identificar los cinco procesos que mas memoria consumen
+	ps aux --sort=-%mem | head -n 6 | tail -n 5
+	#Identificar los cinco procesos que mas CPU concumen
+	ps aux --sort=-%cpu | head -n 6 | tail -n 5
+#Revisar que ninguna particion tenga menos de un 10% de espacio libre
+	# Porcentaje límite de espacio libre
+	Limite=10
 
-# Porcentaje límite de espacio libre
-THRESHOLD=10
+	# Obtener información sobre el uso de las particiones
+	df -h | awk 'NR>1 {print $1, $5, $6}' | while read -r line; do #primero saca informacion de las particiones despues el awk saca el nombre de la particion ($1), el uso ($5) y su ubicaion ($6)
+		Particion=$(echo $line | awk '{print $1}')      # Nombre de la partición
+		Uso=$(echo $line | awk '{print $2}' | tr -d '%')  # Porcentaje de uso (sin '%')
+		Ubicacion=$(echo $line | awk '{print $3}')     # Ubicaion de  la particion
 
-# Obtener información sobre el uso de las particiones
-df -h | awk 'NR>1 {print $1, $5, $6}' | while read -r line; do
-    PARTITION=$(echo $line | awk '{print $1}')      # Nombre de la partición
-    USAGE=$(echo $line | awk '{print $2}' | tr -d '%')  # Porcentaje de uso (sin el símbolo '%')
-    MOUNTPOINT=$(echo $line | awk '{print $3}')     # Punto de montaje
-
-    # Verificar si el uso excede el umbral
-    if [ "$USAGE" -ge $((100 - THRESHOLD)) ]; then
-        echo "Advertencia: La partición $PARTITION montada en $MOUNTPOINT tiene menos del $THRESHOLD% de espacio libre ($USAGE% usado)."
-    fi
-done
+		# Verificar si el uso excede el umbral
+		if [ "$Uso" -ge $((100 - Limite)) ]; then
+			echo "Advertencia: La partición $Particion montada en $Ubicacion tiene menos del $Limite% de espacio libre ($USAGE% usado)."
+		fi
+	done
+#Revisar los archivos syslog y dmesg para detectar errores y eventos criticos
