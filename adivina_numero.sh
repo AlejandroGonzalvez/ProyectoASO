151bd8a057f0ef650d24b3c9672c9a9747747a09
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-18 20:50:47 +0100

    Update adivina_numero.sh

--- adivina_numero.sh
+++ adivina_numero.sh
@@ -3,21 +3,21 @@ Jugando= "0"
 Intentos= "0"
 NumRandom= $(($RANDOM % 100))
 #Mostrar el menu
-while ($jugando -e 0); do
+while [[ $jugando -e 0 ]]; do
 echo "1.Jugar"
 echo "2.Salir"
 read opcion
 case $opcion in
   1)
-    while ($jugando -e 0); do
+    while [[ $jugando -e 0 ]]; do
     echo "Intente adivinar un numero generado de forma random entre el 1 y el 100"
     echo "Introduzca el numero"
     read guess
     (($Intentos++))
-    if ($guess -e $NumRandom); then
+    if [ $guess -e $NumRandom ]; then
       echo "Felicidades lo has conseguido en $Intentos intentos"
       $Jugando= 1 
-    if else ($guess -g $numRandom); then
+    elif [ $guess -g $numRandom ]; then
       echo "Es un numero menor vuelve a intentarlo"
     else
       echo "Es un numero menor vuelve a intentarlo"
