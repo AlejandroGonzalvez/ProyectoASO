4c61b80ac61330453817301ce550200dd5a34301
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-18 19:01:13 +0100

    Update adivina_numero.sh

--- adivina_numero.sh
+++ adivina_numero.sh
@@ -7,12 +7,16 @@ echo "2.Salir"
 read opcion
 case $opcion in
   1)
-    Jugando = 0
-    NumRandom = $(($RANDOM % 100))
+    Jugando= 0
+    Intentos= 0
+    NumRandom= $(($RANDOM % 100))
     while ($jugando e 0); do
     echo "Intente adivinar un numero generado de forma random entre el 1 y el 100"
     echo "Introduzca el numero"
     read guess
+    ($Intentos++)
+    if ($guess e $NumRandom) then
+      echo "Felicidades lo has conseguido en $Intentos intentos"
   ;;
   2)
     echo "Adios, vuelva pronto"
