8ae3e2033fbf7f6ca605a7d0c93cdfa3d27767e4
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-18 19:07:22 +0100

    Update adivina_numero.sh

--- adivina_numero.sh
+++ adivina_numero.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 # Leer el uidNumber del archivo externo
-uidNumber=$(<"$uid_file")
+Jugando= 0
+Intentos= 0
+NumRandom= $(($RANDOM % 100))
 #Mostrar el menu
+
 echo "1.Jugar"
 echo "2.Salir"
 read opcion
 case $opcion in
   1)
-    Jugando= 0
-    Intentos= 0
-    NumRandom= $(($RANDOM % 100))
     while ($jugando e 0); do
     echo "Intente adivinar un numero generado de forma random entre el 1 y el 100"
     echo "Introduzca el numero"
     read guess
-    ($Intentos++)
+    (($Intentos++))
     if ($guess e $NumRandom) then
       echo "Felicidades lo has conseguido en $Intentos intentos"
+      $Jugando= 1 
+    if else ($guess g $numRandom) then
+      echo "Es un numero menor vuelve a intentarlo"
+    else
+      echo "Es un numero menor vuelve a intentarlo"
+    fi
   ;;
   2)
-    echo "Adios, vuelva pronto"
+    echo "Gracias por jugar. ¡Hasta pronto!”"
   ;;
   *)
-    echo "Opcion invalida saliendo del juego"
+    echo "Opción no válida. Por favor, selecciona 1 o 2."
   ;;
