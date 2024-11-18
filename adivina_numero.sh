27123ef97a4b2dc2244213122dbad3f8608deb69
AlejandroGonzalvez <4108807@alu.murciaeduca.es>
2024-11-18 18:16:10 +0100

    Update adivina_numero.sh

--- adivina_numero.sh
+++ adivina_numero.sh
@@ -4,3 +4,14 @@ uidNumber=$(<"$uid_file")
 #Mostrar el menu
 echo "1.Jugar"
 echo "2.Salir"
+read opcion
+case $opcion in
+  1)
+    
+  ;;
+  2)
+    echo "Adios, vuelva pronto"
+  ;;
+  *)
+    echo "Opcion invalida saliendo del juego"
+  ;;
